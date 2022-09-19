69f82815679faddf70b358f75699f8a88fe9a241
fida <fidaktk@gmail.com>
2022-09-19 16:52:23 +0500

    no message

--- dist/fabric.js
+++ dist/fabric.js
@@ -1,7 +1,7 @@
 /* build: `node build.js modules=ALL minifier=uglifyjs` */
 /*! Fabric.js Copyright 2008-2015, Printio (Juriy Zaytsev, Maxim Chernyak) */
 
-var fabric = fabric || { version: '3.7.3' };
+var fabric = fabric || { version: '3.7.4' };
 if (typeof exports !== 'undefined') {
   exports.fabric = fabric;
 }
